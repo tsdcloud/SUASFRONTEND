4530fd29fe6c745771ab038d09820cedfcec0367
Isclif <isclifben@gmail.com>
2025-06-20 16:02:25 +0100

    stable version v1.3.4

--- src/Pages/AssignRoleEventToUser.jsx
+++ src/Pages/AssignRoleEventToUser.jsx
@@ -125,16 +125,16 @@ const AssignRoleEventToUser = ({ fetchData }) => {
             const res = await handlePost(url, data);
 
             if (res.success) {
-                toast.success("nouveau role assignée avec succès", { duration: 1500 })
+                toast.success("nouveau role assignée avec succès", { duration: 2000 })
                 setTimeout(() => {
                     // navigateTo('/categories');
                     fetchData()
-                }, 1500);
+                }, 2000);
 
                 // navigateTo("/categories")
             }
             else {
-                toast.error("une erreur est survenue...", { duration: 5000 });
+                toast.error(res.message, { duration: 5000 });
                 return;
             }
         }
